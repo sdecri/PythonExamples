d362c444266006ac166fd8d4dbf79cb4afa0b8a5
simone <simone.decristofaro85@gmail.com>
2015-01-02 12:58:59 +0100

    Aggiunto flask

--- flaskTest.py
+++ flaskTest.py
@@ -1,1 +1,33 @@
 __author__ = 'simone.decristofaro'
+
+from flask import Flask, request
+
+app=Flask(__name__)
+
+@app.route('/')
+@app.route('/hello')
+def hello():
+    return sayHello('World')
+
+@app.route('/hello/<name>')
+def sayHello(name):
+    """
+    Say hello to a person
+    :param str name:
+    :return:
+    """
+    return 'Hello ' + name
+
+@app.route('/methodType', methods=['GET', 'POST'])
+def methodType():
+    if request.method == 'POST':
+        return 'The method is: POST'
+    else:
+        return 'The method is: GET'
+
+
+def main():
+    app.run(debug=True)
+
+if __name__=='__main__':
+    main()
